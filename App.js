3fff68e46f62805872664df8d5f5f2b43c61aca0
Abdul Javith <abduljavithgit@gmail.com>
2025-05-02 17:41:26 +0530

    laying the foundation functional component

--- App.js
+++ App.js
@@ -2,8 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("h1",{id:"heading"},"hello world from react");
+
+
+//const heading = React.createElement("h1",{id:"heading"},"hello world from react");
+// recat element 
+const Title = () => (<h1 id="heading">hello javit this react learning</h1>)
+// react component
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <Title></Title>
+        {Title()}
+        <h1 className="heading">hello javith what are you doing?</h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
 //ReactDOM.hydrateRoot(document.getElementById("root"), heading);
+
+
 
